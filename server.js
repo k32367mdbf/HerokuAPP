6ce4bc0d421eb281ce95637f869da7f2e9bb8ab7
Ziwei <k32367mdbf@yahoo.com.tw>
2017-06-26 13:41:35 +0800

    SeeMarket

--- server.js
+++ server.js
@@ -16,24 +16,10 @@ var mongoose = require('mongoose');
 var USER_PASSWD = process.argv[2];
 
 
-var lex = LEX.create({
-	configDir: require('os').homedir() + '/letsencrypt/etc'
-	, approveRegistration: function (hostname, cb) { // leave `null` to disable automatic registration
-		// Note: this is the place to check your database to get the user associated with this domain
-		cb(null, {
-			domains: [hostname]
-			, email: 'st9007a@gmail.com' // user@example.com
-			, agreeTos: true
-		});
-	}
-});
-
 app.use(express.static(__dirname + '/public'));
 
-lex.onRequest = app;
+http.listen(process.env.PORT || 2017);
 
-
-https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app)).listen(process.env.PORT || 2017);
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true , limit: '50mb'}));
 app.use(express.static(__dirname + '/public'));
